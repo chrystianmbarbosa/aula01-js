e1bd47c7dd50cfe00cd67780d773c942039c33b7
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-08-20 21:45:34 -0300

    feat: Adicionado sistema de notas JS

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+//Comentário em Linha
+//Exibir no terminal
 console.log("Olá Mundo");
-
+//Exibir no navegador
 document.write("Olá Navegador!");
+ 
+/*
+    Comentário de Bloco
+    cljkhcxzkjcjkhcz
+    zxczxczxc
+    alert("Alerta de Confirmação!");
+*/
+
+// Capturar o nome do Aluno
+var nome = window.prompt("Informe o Nome do Aluno:");
+// Guardar a primeira nota
+var nota1 = window.prompt("Digite a Nota 1:");
+//converte a nota1 de texto para decimal
+nota1 = parseFloat(nota1);
+// Guardar a segunda nota
+var nota2 = window.prompt("Digite a Nota 2:");
+//converte a nota2 de texto para decimal
+nota2 = parseFloat(nota2);
+//calculo a média das duas notas
+var media = (nota1 + nota2)/2;
+//Gero a mensagem de exibição do nome e da média do aluno
+var mensagem = nome + ", você obteve a média : " + media; 
+window.alert(mensagem);
