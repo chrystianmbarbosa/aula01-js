05f6729ae2d03fb88e9e513e62a652e7965df5f4
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-08-21 21:35:02 -0300

    feat: Correção da média dos 2 alunos

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-//Comentário em Linha
-//Exibir no terminal
-console.log("Olá Mundo");
-//Exibir no navegador
-document.write("Olá Navegador!");
- 
-/*
-    Comentário de Bloco
-    cljkhcxzkjcjkhcz
-    zxczxczxc
-    alert("Alerta de Confirmação!");
+/* 
+Faça um algoritmo onde o usuário informe o nome 
+e 4 notas de dois alunos e mostre as 
+médias desses 2 alunos.
 */
+var nome1, nome2 = "";
+var nota1, nota2, nota3, nota4,
+    nota5, nota6, nota7, nota8 = 0;
+const quantidadeProvas = 4;
 
-// Capturar o nome do Aluno
-var nome = window.prompt("Informe o Nome do Aluno:");
-// Guardar a primeira nota
-var nota1 = window.prompt("Digite a Nota 1:");
-//converte a nota1 de texto para decimal
-nota1 = parseFloat(nota1);
-// Guardar a segunda nota
-var nota2 = window.prompt("Digite a Nota 2:");
-//converte a nota2 de texto para decimal
-nota2 = parseFloat(nota2);
-//calculo a média das duas notas
-var media = (nota1 + nota2)/2;
-//Gero a mensagem de exibição do nome e da média do aluno
-var mensagem = nome + ", você obteve a média : " + media; 
-window.alert(mensagem);
+nome1 = prompt("Digite o Nome do Aluno 1:");
+nota1 = parseFloat(prompt("Digite a Nota 1 do Aluno 1: "));
+nota2 = parseFloat(prompt("Digite a Nota 2 do Aluno 1: "));
+nota3 = parseFloat(prompt("Digite a Nota 3 do Aluno 1: "));
+nota4 = parseFloat(prompt("Digite a Nota 4 do Aluno 1: "));
+
+nome2 = prompt("Digite o Nome do Aluno 2:");
+nota5 = parseFloat(prompt("Digite a Nota 1 do Aluno 2: "));
+nota6 = parseFloat(prompt("Digite a Nota 2 do Aluno 2: "));
+nota7 = parseFloat(prompt("Digite a Nota 3 do Aluno 2: "));
+nota8 = parseFloat(prompt("Digite a Nota 4 do Aluno 2: "));
+
+var media1, media2 = 0;
+media1 = (nota1 + nota2 + nota3 + nota4) / quantidadeProvas;
+media2 = (nota5 + nota6 + nota7 + nota8) / quantidadeProvas;
+
+var mensagem = `
+${nome1}, sua média é ${media1}, parabéns!   
+${nome2}, sua média é ${media2}, parabéns!`;
+
+alert(mensagem);
